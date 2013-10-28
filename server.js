e20fd9d28e646531644dc4ba1256ede8b6a3bfe5
Brian Green <briangreenery@gmail.com>
2013-10-27 20:28:08 -0700

    slight cleanup

--- server.js
+++ server.js
@@ -2,30 +2,31 @@ var fs = require('fs');
 var path = require('path');
 var ejs = require('ejs');
 var express = require('express');
+
 var app = express();
 
 var randomName = function(length) {
-	var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-	var name = '';
-	for (var i = 0; i < length; i++) {
-		var number = Math.floor(Math.random() * alphabet.length);
-		name += alphabet[number];
-	}
+  var name = '';
+  for (var i = 0; i < length; i++) {
+    var number = Math.floor(Math.random() * alphabet.length);
+    name += alphabet[number];
+  }
 
-	return name;
+  return name;
 }
 
 var tmpDir = path.join(__dirname, 'tmp');
 var uploadsDir = path.join(__dirname, 'uploads');
 
 if (!fs.existsSync(tmpDir))
-	fs.mkdirSync(tmpDir);
+  fs.mkdirSync(tmpDir);
 
 if (!fs.existsSync(uploadsDir))
-	fs.mkdirSync(uploadsDir);
+  fs.mkdirSync(uploadsDir);
 
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, '/views'));
 app.engine('html', ejs.renderFile);
 
 app.use(express.limit('100 gb'));
@@ -33,24 +34,23 @@ app.use(express.bodyParser({uploadDir: tmpDir}));
 app.use('/share', express.static(uploadsDir));
 app.use('/share', express.static(path.join(__dirname, 'deps')));
 
-app.get('/share', function (req, res)
-{
-	res.render('index.html');
+app.get('/share', function (req, res) {
+  res.render('index.html');
 });
 
 app.post('/upload', function (req, res) {
-	var folder = randomName(5);
-	var name = req.files.file.name;
+  var folder = randomName(5);
+  var name = req.files.file.name;
 
-	fs.mkdirSync(path.join(uploadsDir, folder));
-	fs.renameSync(req.files.file.path, path.join(uploadsDir, folder, name));
+  fs.mkdirSync(path.join(uploadsDir, folder));
+  fs.renameSync(req.files.file.path, path.join(uploadsDir, folder, name));
 
-	res.send(folder + '/' + encodeURIComponent(name));
+  res.send(folder + '/' + encodeURIComponent(name));
 });
 
 app.use(function (req, res, next) {
-	res.status(404);
-	res.render('404.html');
+  res.status(404);
+  res.render('404.html');
 });
 
 app.listen(80);
